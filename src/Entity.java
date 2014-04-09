306133dc7d1502a9aac666624f2bcfd6b28befd0
Reaver <ellis.neo@gmail.com>
2014-04-09 11:01:15 +0300

    change load function for a new architecture

--- src/Entity.java
+++ src/Entity.java
@@ -1,14 +1,16 @@
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Entity {
+public abstract class Entity {
 	private int id;
-
+	private List<String> values = new ArrayList<String>();
+	private boolean isLoaded = false;
+	
+	
 	public Entity(int id) {
 		this.id = id;
 	}
@@ -17,10 +19,13 @@ public class Entity {
 		return id;
 	}
 	
+	public abstract List<String> getFields();
+	
 	public void load() {
 		Connection connection = Postgresql.getConnection();
 		String className = this.getClass().getSimpleName().toLowerCase();
-		Field[] fields = this.getClass().getDeclaredFields();
+		List<String> fields = this.getFields();
+		int index;
 		
 		try {
 			PreparedStatement preparedStatement 
@@ -32,39 +37,40 @@ public class Entity {
 			result = preparedStatement.executeQuery();
 			
 			result.next();
-			for ( Field field : fields ) {
-				String methodName = "set" 
-						+ capitalizeFirstLetter(field.getName());
-				Method method = this.getClass().getMethod(methodName,
-						field.getType());
-				
-				method.invoke(this, result.getString(className + "_" 
-						+ field.getName()));
+			for ( String field : fields ) {
+				index = fields.indexOf(field);
+				values.add(index, result.getString(className + "_" + field));
 			}
-		} catch (NoSuchMethodException e) {
-			System.out.println("No set method for field");
-		} catch (SecurityException | IllegalAccessException | SQLException 
-				| InvocationTargetException | IllegalArgumentException e) {
+		
+		isLoaded = true;
+		} catch (SecurityException | SQLException 
+				| IllegalArgumentException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	private String capitalizeFirstLetter(String original){
-	    if(original.length() == 0)
-	        return original;
-	    return original.substring(0, 1).toUpperCase() + original.substring(1);
+	public String getValue(String fieldName) {
+		int index = this.getFields().indexOf(fieldName);
+		
+		if ( index == -1 ) {
+			throw new IllegalArgumentException(); 
+		} else if ( !isLoaded ) {
+			this.load();
+		}
+		
+		return values.get(index);
 	}
 	
 	public static void main(String[] args) {
 		Article at = new Article(1);
-		Category cat = new Category(1);
-		Tag tag = new Tag(2);
+		//Category cat = new Category(1);
+		//Tag tag = new Tag(2);
 		
-		System.out.println(at.getTitle());
-		System.out.println(at.getText());
+		//System.out.println(cat.getTitle());
+		//System.out.println(tag.getValue());
 		
-		System.out.println(cat.getTitle());
-		System.out.println(tag.getValue());
+		System.out.println(at.getValue("title"));
+		System.out.println(at.getValue("text"));
 		
 		Postgresql.closeConnection();
 	}
